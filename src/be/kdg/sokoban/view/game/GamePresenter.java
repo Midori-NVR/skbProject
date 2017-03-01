077fc07af9b2d92e8ec457fec196551383c467fb
scarfader <vdhlies@gmail.com>
2017-03-01 15:53:20 +0100

    partially added move function added css file for game

--- src/be/kdg/sokoban/view/game/GamePresenter.java
+++ src/be/kdg/sokoban/view/game/GamePresenter.java
@@ -5,6 +5,7 @@ import be.kdg.sokoban.model.Objects.FieldObject;
 import be.kdg.sokoban.model.SokobanModel;
 import javafx.scene.input.KeyCode;
 
+
 /**
  * @author Niels Van Reeth
  * @version 1.0 2/21/2017 11:43 AM
@@ -37,16 +38,13 @@ public class GamePresenter {
     }
 
     private void addStyleSheets() {
-        view.setStyle("-fx-background-image: url(\"be/kdg/sokoban/view/game/images/floor.png\")");
+        view.getStylesheets().add("be/kdg/sokoban/view/game/css/game.css");
+        view.getStyleClass().add("body");
     }
 
     public void addWindowEventHandlers() {
-        view.getScene().widthProperty().addListener(observable -> {
-            updateView();
-        });
-        view.getScene().heightProperty().addListener(observable -> {
-            updateView();
-        });
+        view.getScene().widthProperty().addListener(observable -> updateView());
+        view.getScene().heightProperty().addListener(observable -> updateView());
         view.getScene().setOnKeyPressed(event -> {
             if (event.getCode().equals(KeyCode.LEFT)) {
                 model.move(FieldObject.MOVE_LEFT);
